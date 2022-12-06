e8887053e9a946403922a4871707b63329a796a2
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-06 11:27:13 +0100

    new images and timeline

---
+++ src/_data/team.js
@@ -0,0 +1,98 @@
+module.exports = [
+    {
+        name: "Ralf Usbeck",
+        position: "CEO",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Manuel Saballus",
+        position: "VP Commercial",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Daniel Zingsheim",
+        position: "VP Operations",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Benjamin Rose",
+        position: "VP IT & Infrastructure",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Sebastian Nölke",
+        position: "VP Supply Solutions",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Christopher Rütten",
+        position: "VP Production Solutions",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Guido Fünders",
+        position: "Vice President Distribution Solutions",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Paul Weskamp",
+        position: "Vice President Finance & HR",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Sebastian Resing",
+        position: "Head of Technical Account Management",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Axel Brand",
+        position: "Director Key Account Management",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Dimo Pashov",
+        position: "Head of Sales EMEA",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Christian Andersen",
+        position: "Director Flight",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Laura Garcia",
+        position: "Commercial Lead Accommodation & Meta Search",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Christian Liebig",
+        position: "Head of Accommodation & Ancillaries",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Peter Cramer",
+        position: "Director IT Procurement",
+        bio: "",
+        linkedin: ""
+    },
+    {
+        name: "Illa Eggers",
+        position: "PR & Marketing Manager",
+        bio: "",
+        linkedin: ""
+    }
+]
