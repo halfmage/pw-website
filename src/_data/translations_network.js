e84e5966b87787ed4daeadd6b29d732fcfbd3135
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-25 15:02:56 +0100

    hero styles, network logos and translations

---
+++ src/_data/translations_network.js
@@ -0,0 +1,10 @@
+module.exports = {
+    en: {
+        text_google: 'Google and Peakwork are in cooperation exploring travel product distribution and search technology methodology.',
+        text_drv: 'Peakwork is the service provider for the German Travel Association (DRV) defining DRV GlobalTypes® and DRV Data Standard.'
+    },
+    de: {
+        text_google: 'Google und Peakwork arbeiten gemeinsam an der Erforschung der Methodik des Reiseproduktvertriebs und der Suchtechnologie.',
+        text_drv: 'Peakwork ist der Dienstleister für den Deutschen Reiseverband (DRV) und definiert DRV GlobalTypes® und DRV Data Standard.'
+    }
+};
