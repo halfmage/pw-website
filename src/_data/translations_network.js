df44f34f6188afef57bdf2990da9105bbcf8a934
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-25 18:12:26 +0100

    network page updates

--- src/_data/translations_network.js
+++ src/_data/translations_network.js
@@ -1,10 +1,22 @@
 module.exports = {
     en: {
+        cooperations: 'Cooperation Partners',
+        partner: 'Distribution Partners',
         text_google: 'Google and Peakwork are in cooperation exploring travel product distribution and search technology methodology.',
-        text_drv: 'Peakwork is the service provider for the German Travel Association (DRV) defining DRV GlobalTypes® and DRV Data Standard.'
+        text_drv: 'Peakwork is the service provider for the German Travel Association (DRV) defining DRV GlobalTypes® and DRV Data Standard.',
+        text_freshcells: 'Düsseldorf based freshcells develops website and mobile app frontends based on Peakwork technology.',
+        text_netmatch: 'Distribution partner for BeNeLux. Amsterdam, Netherlands',
+        text_luckytrip: 'Distribution partner for China. Chongqing, China',
+        text_riversoft: 'Distribution partner for Taiwan. Taipei City, Taiwan',
     },
     de: {
+        cooperations: 'Kooperationspartner',
+        partner: 'Vertriebspartner',
         text_google: 'Google und Peakwork arbeiten gemeinsam an der Erforschung der Methodik des Reiseproduktvertriebs und der Suchtechnologie.',
-        text_drv: 'Peakwork ist der Dienstleister für den Deutschen Reiseverband (DRV) und definiert DRV GlobalTypes® und DRV Data Standard.'
+        text_drv: 'Peakwork ist der Dienstleister für den Deutschen Reiseverband (DRV) und definiert DRV GlobalTypes® und DRV Data Standard.',
+        text_freshcells: 'Das Düsseldorfer Unternehmen freshcells entwickelt Website- und mobile App-Frontends auf Basis der Peakwork-Technologie.',
+        text_netmatch: 'Vertriebspartner für BeNeLux. Amsterdam, Niederlande',
+        text_luckytrip: 'Vertriebspartner für China. Chongqing, China',
+        text_riversoft: 'Vertriebspartner für Taiwan. Taipei-Stadt, Taiwan',
     }
 };
